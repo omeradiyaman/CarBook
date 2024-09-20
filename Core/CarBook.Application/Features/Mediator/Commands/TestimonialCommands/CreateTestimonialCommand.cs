e2d9862939640116a0f4ab0ee1539af16bd70f6a
Ömer A <omeradiyaman1169@gmail.com>
2024-09-20 11:49:46 +0300

    Testimonial Mediator done

---
+++ Core/CarBook.Application/Features/Mediator/Commands/TestimonialCommands/CreateTestimonialCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Commands.TestimonialCommands
+{
+    public class CreateTestimonialCommand : IRequest
+    {
+        public string Name { get; set; }
+        public string Tİtle { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Core/CarBook.Application/Features/Mediator/Commands/TestimonialCommands/UpdateTestimonialCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Commands.TestimonialCommands
+{
+    public class UpdateTestimonialCommand : IRequest
+    {
+        public int TestimonialId { get; set; }
+        public string Name { get; set; }
+        public string Tİtle { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
