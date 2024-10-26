1435d9c3ebd694a40563fdcc0c5281359ae041a9
Ömer A <omeradiyaman1169@gmail.com>
2024-10-26 13:36:02 +0300

    register - login

--- Frontend/CarBook.Dto/RegisterDtos/CreateRegisterDto.cs
+++ Frontend/CarBook.Dto/RegisterDtos/CreateRegisterDto.cs
@@ -4,17 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarBook.Domain.Entities
+namespace CarBook.Dto.RegisterDtos
 {
-	public class AppUser
+	public class CreateRegisterDto
 	{
-        public int AppUserId { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
-        public int AppRoleId { get; set; }
-        public AppRole AppRole { get; set; }
     }
 }
